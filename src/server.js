9dfc981d95e3d0232a1018f67f95708038854a90
martins1979 <martins.1979@hotmail.com>
2024-01-31 13:53:37 -0300

    altera o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("boas vindas a api do allbooks")
+  console.log("API disponível atraves da url http://localhost:8000")
 })
